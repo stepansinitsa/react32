519b79ab8d9095ba1babef91f1c94aacfe6288f6
stepan.sinitsa <stepansinitsa@gmail.com>
2024-11-06 18:04:21 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Listing from './components/Listing';
+import etsy from './data/etsy';
+
+function App() {
+  return (
+    <div className="App">
+      <Listing items={etsy} />
+    </div>
+    );
+}
+
+export default App;
